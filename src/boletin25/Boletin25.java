dc06de02d61287f6594f46d473d2fb9959ea3ca5
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2019-04-01 12:15:26 +0200

    Terminado

---
+++ src/boletin25/Boletin25.java
@@ -0,0 +1,19 @@
+
+
+package boletin25;
+
+/**
+ *
+ * @author clamascabaleiro
+ */
+public class Boletin25 {
+
+ 
+    public static void main(String[] args) {
+       Paleta1 ventana1 = new Paleta1();
+       Paleta2 ventana2 = new Paleta2();
+       ventana1.setVisible(true);
+       ventana2.setVisible(true);
+    }
+
+}
